f3ec856c691e68e70eaa2c09bb3a660f9492dedc
Leochen018 <leochen018@outlook.com>
2026-04-04 22:43:11 +0100

    Update API URL to Railway production

--- files/ios/App/App/public/_next/static/x9nO0Suh382efLv-aJxRM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/6e8d85aa-b249c29896bb4cf9.js","static/chunks/561-8bce095283d646cb.js","static/chunks/pages/index-3f1232b9d3ade9fb.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
