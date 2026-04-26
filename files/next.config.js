8460cd1ad475068bdfac4cc2fe2062b0c8f7cee0
Leochen018 <leochen018@outlook.com>
2026-04-26 01:00:03 +0100

    fix package.json dependencies

--- files/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // <--- This MUST be exactly this
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-}
-module.exports = nextConfig
